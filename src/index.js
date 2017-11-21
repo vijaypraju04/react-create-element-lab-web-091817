a84641417b65278a4b0539c2109dae43319ac615
Vijay Raju <vijaypraju@gmail.com>
2017-11-21 00:47:06 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -10,3 +10,26 @@ ReactDOM.render(
 
 export default meInReact
 */
+
+const meInReact =
+React.createElement("div", { className: "me" },
+  React.createElement("h1", {}, "An Awesome Person"),
+  React.createElement("p", {}, "Who is learning React"),
+
+  React.createElement("ul", { className: "my-interests" },
+    [
+      React.createElement("li", {}, "JavaScript"),
+      React.createElement("li", {}, "React"),
+      React.createElement("li", {}, "Movies"),
+      React.createElement("li", {}, "Ice cream")
+    ]
+  )
+);
+
+
+ReactDOM.render(
+  meInReact,
+  document.getElementById('global')
+);
+
+export default meInReact;
